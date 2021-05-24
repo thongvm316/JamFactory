e34fc69bc51102d5ae0b538b5808b6bc4c2ae1ea
ThongVM <vominhthong1603@gmail.com>
2021-05-24 13:08:01 +0700

    change config firebase

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,12 +1,22 @@
 import firebase from 'firebase'
 
+// const config = {
+//   apiKey: 'AIzaSyB70g0uapJVx6g7pHyCYi3fYJGwyQqeQE0',
+//   authDomain: 'fir-reactjs-f8ec1.firebaseapp.com',
+//   projectId: 'fir-reactjs-f8ec1',
+//   storageBucket: 'fir-reactjs-f8ec1.appspot.com',
+//   messagingSenderId: '934477481370',
+//   appId: '1:934477481370:web:3e67fcffaa6c128d3da623',
+// }
+
 const config = {
-  apiKey: 'AIzaSyB70g0uapJVx6g7pHyCYi3fYJGwyQqeQE0',
-  authDomain: 'fir-reactjs-f8ec1.firebaseapp.com',
-  projectId: 'fir-reactjs-f8ec1',
-  storageBucket: 'fir-reactjs-f8ec1.appspot.com',
-  messagingSenderId: '934477481370',
-  appId: '1:934477481370:web:3e67fcffaa6c128d3da623',
+  apiKey: 'AIzaSyA4YydViLaSlk4TVw8M2nAO-utRsoOBYYk',
+  authDomain: 'gembox-410c0.firebaseapp.com',
+  projectId: 'gembox-410c0',
+  storageBucket: 'gembox-410c0.appspot.com',
+  messagingSenderId: '1028767831028',
+  appId: '1:1028767831028:web:af053b416d8004214d5fa1',
+  measurementId: 'G-W2WW1WC3P6',
 }
 
 firebase.initializeApp(config)
